6229f4c682702365dc704777c36d8ddad0f690f5
Implement Experiments <experiments@implement.dev>
2026-02-10 02:18:38 +0000

    Experiment: golden-wood-73

--- train.py
+++ train.py
@@ -192,16 +192,16 @@ def read_prompts(
 
 
 def main():
-    seed = 42
+    seed = 45
     wandb_project = "tiny_grpo"
     device_index = 0
     model_name = "meta-llama/Llama-3.2-1B-Instruct"
     checkpoint_path = Path("./output")
     checkpoint_interval = 20
-    train_batch_size = 16
+    train_batch_size = 32
     lr = 5e-6
-    kl_weight = 0.01
-    clip_eps = 0.2
+    kl_weight = 0.1
+    clip_eps = 0.3
 
     group_size = 12
     rollouts_per_step = 32
