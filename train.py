c1453caccc966014cc8544b528d83349cb8582c7
Implement Experiments <experiments@implement.dev>
2026-02-10 00:53:10 +0000

    Experiment: stoic-salad-66

--- train.py
+++ train.py
@@ -198,13 +198,13 @@ def main():
     model_name = "meta-llama/Llama-3.2-1B-Instruct"
     checkpoint_path = Path("./output")
     checkpoint_interval = 20
-    train_batch_size = 16
-    lr = 5e-6
-    kl_weight = 0.01
-    clip_eps = 0.2
+    train_batch_size = 8
+    lr = 1e-6
+    kl_weight = 0
+    clip_eps = 0.25
 
     group_size = 12
-    rollouts_per_step = 32
+    rollouts_per_step = 16
     epochs_per_step = 1
     max_norm = 1.0  # gradient clipping
 
