5ef31b9a8f6a6362fce6e2dc6e2dcbad3c2ca987
Rehaan Ahmad <rehaanahmad03@gmail.com>
2026-02-26 03:09:22 +0000

    kl 0

--- train.py
+++ train.py
@@ -209,10 +209,10 @@ def main():
     checkpoint_interval = 20
     train_batch_size = 16
     lr = 5e-6
-    kl_weight = 0.01
+    kl_weight = 0
     clip_eps = 0.2
 
-    group_size = 48
+    group_size = 12
     rollouts_per_step = 32
     epochs_per_step = 1
     max_norm = 1.0  # gradient clipping
